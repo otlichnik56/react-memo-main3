63ac0e892cfe928415a5bdb0aba6bc817d490f65
otlichnik56 <otlichnik56@yandex.ru>
2024-07-04 19:02:43 +0500

    12

---
+++ src/components/EndGameModal/EndGameModal.jsx
@@ -0,0 +1,27 @@
+import styles from "./EndGameModal.module.css";
+
+import { Button } from "../Button/Button";
+
+import deadImageUrl from "./images/dead.png";
+import celebrationImageUrl from "./images/celebration.png";
+
+export function EndGameModal({ isWon, gameDurationSeconds, gameDurationMinutes, onClick }) {
+  const title = isWon ? "Вы победили!" : "Вы проиграли!";
+
+  const imgSrc = isWon ? celebrationImageUrl : deadImageUrl;
+
+  const imgAlt = isWon ? "celebration emodji" : "dead emodji";
+
+  return (
+    <div className={styles.modal}>
+      <img className={styles.image} src={imgSrc} alt={imgAlt} />
+      <h2 className={styles.title}>{title}</h2>
+      <p className={styles.description}>Затраченное время:</p>
+      <div className={styles.time}>
+        {gameDurationMinutes.toString().padStart("2", "0")}.{gameDurationSeconds.toString().padStart("2", "0")}
+      </div>
+
+      <Button onClick={onClick}>Начать сначала</Button>
+    </div>
+  );
+}
---
+++ src/pages/SelectLevelPage/SelectLevelPage.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import styles from "./SelectLevelPage.module.css";
+
+export function SelectLevelPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        <h1 className={styles.title}>Выбери сложность</h1>
+        <ul className={styles.levels}>
+          <li className={styles.level}>
+            <Link className={styles.levelLink} to="/game/3">
+              1
+            </Link>
+          </li>
+          <li className={styles.level}>
+            <Link className={styles.levelLink} to="/game/6">
+              2
+            </Link>
+          </li>
+          <li className={styles.level}>
+            <Link className={styles.levelLink} to="/game/9">
+              3
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
